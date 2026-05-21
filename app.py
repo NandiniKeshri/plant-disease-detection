9a473aa452d46edabe8a3775173184a1f725cef3
Your Name <your-email@example.com>
2026-05-22 00:45:58 +0530

    Command changes

--- app.py
+++ app.py
@@ -102,5 +102,4 @@ def predict():
 # RUN (FOR LOCAL ONLY)
 # ----------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0")
